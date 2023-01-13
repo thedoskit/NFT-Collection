662d47106c5e1ffad9507764b78d1735914381c6
Kazeem Dosunmu <dosunmukazeem1@gmail.com>
2023-01-14 00:02:00 +0100

    nft commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xfc760CA913ee112aE1DC3294761ab8A3f5e9d18D";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
